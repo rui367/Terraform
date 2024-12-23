1a7080b01021cbc25460937687f6347113f37092
rui367 <ruifaria_11@hotmail.com>
2024-12-23 14:53:50 +0000

    commit main file

---
+++ main.tf
@@ -0,0 +1,57 @@
+# Reference the existing resource group
+resource "azurerm_resource_group" "rg" {
+  name = "ACCIPIENSTEST-UK-AZCLD-S-DEWEU-RG-001-VWFS-RGP"
+  location = "Germany West Central"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subvnet" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  count = 3
+  name                = "example-nic-${count.index}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subvnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "VM" {
+  count = 3
+  name                  = "example-vm-${count.index}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+  azurerm_network_interface.nic[count.index].id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-datacenter-azure-edition-hotpatch"
+    version   = "latest"
+  }
+}
